592fb2078ef8b816c109b1684e7eb253baaff631
the6p4c <me@doggirl.gay>
2019-12-27 14:39:00 +1000

    Support numeric format parameters

--- rust-fmt-game-wasm/src/lib.rs
+++ rust-fmt-game-wasm/src/lib.rs
@@ -1,6 +1,5 @@
 mod utils;
 
-#[macro_use]
 use runtime_fmt::*;
 use wasm_bindgen::prelude::*;
 
@@ -15,21 +14,20 @@ extern "C" {
     fn alert(s: &str);
 }
 
-fn js_list_to_string_vec(l: Box<[JsValue]>) -> Option<Vec<String>> {
-    // Since there's no way I know of to take a Vec of Option and transpose it,
-    // we'll take a round-trip through Result to do the same operation.
-    // Vec<Option<T>>
-    // -> Vec<Result<T, ()>>
-    // -> Result<Vec<T>, ()>
-    // -> Option<Vec<T>>
-    l.iter()
-        .map(JsValue::as_string)
-        .map(|v| match v {
-            Some(x) => Ok(x),
-            None => Err(()),
-        })
-        .collect::<Result<Vec<String>, ()>>()
-        .ok()
+enum ParamFromJs {
+    Str(String),
+    Integer(u32),
+}
+
+impl ParamFromJs {
+    fn as_param(&self) -> Param<'_> {
+        use ParamFromJs::*;
+
+        match self {
+            Str(s) => Param::normal(s),
+            Integer(i) => Param::normal(i),
+        }
+    }
 }
 
 fn error_to_jsvalue(e: Error) -> JsValue {
@@ -38,17 +36,34 @@ fn error_to_jsvalue(e: Error) -> JsValue {
 
 #[wasm_bindgen]
 pub fn format(spec: &str, params: Box<[JsValue]>) -> Result<String, JsValue> {
-    let params_strings = match js_list_to_string_vec(params) {
-        Some(x) => x,
-        None => return Err(JsValue::from_str("non-string param")),
+    // Convert each formatting parameter to a Rust-usable type
+    let params = params
+        .iter()
+        .map(|jsv| {
+            if let Some(s) = jsv.as_string() {
+                return Ok(ParamFromJs::Str(s));
+            }
+
+            if let Some(f) = jsv.as_f64() {
+                if f == f.floor() {
+                    return Ok(ParamFromJs::Integer(f as u32));
+                } else {
+                    return Err(String::from("numeric param was not integer"));
+                }
+            }
+
+            return Err(String::from("invalid param type"));
+        })
+        .collect::<Result<Vec<ParamFromJs>, String>>();
+
+    // If any parameter conversion failed, fail the whole call
+    let params = match params {
+        Ok(params) => params,
+        Err(e) => return Err(JsValue::from_str(&e)),
     };
 
-    let params = params_strings
-        .iter()
-        .map(|s| Param::normal(s))
-        .collect::<Vec<_>>();
-
-    let format_buf = FormatBuf::new(spec, &params).map_err(|e| error_to_jsvalue(e))?;
+    let format_params = params.iter().map(ParamFromJs::as_param).collect::<Vec<_>>();
+    let format_buf = FormatBuf::new(spec, &format_params).map_err(|e| error_to_jsvalue(e))?;
 
     Ok(format_buf.format())
 }
