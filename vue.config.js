fb4aaf6702186b7578752b75cf340eaf106e25c5
the6p4c <me@doggirl.gay>
2019-12-28 18:46:43 +1000

    Inform Vue/Webpack of the WASM component

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+const path = require('path');
+
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+
 module.exports = {
 	// Deployed at https://the6p4c.github.io/rust-fmt-game/
-	publicPath: process.env.NODE_ENV === 'production' ? '/rust-fmt-game' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/rust-fmt-game' : '/',
+	configureWebpack: {
+		plugins: [
+			new WasmPackPlugin({
+				crateDirectory: path.join(__dirname, 'rust-fmt-game-wasm'),
+				outDir: path.join(__dirname, 'rust-fmt-game-wasm', 'pkg')
+			})
+		]
+	}
 };
